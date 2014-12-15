f0642da67a331af8f3daacd1df9b9944967fa45b
cproduct <heier.micha@web.de>
2014-12-15 09:28:33 +0100

    benchmark first implemention.
    missing: different colors, dimensions, ...

--- raytracer_test.go
+++ raytracer_test.go
@@ -8,33 +8,34 @@ import (
 	objects "de/vorlesung/projekt/raytracer/SceneObjects"
 	"log"
 	"os"
+	"path"
 	"runtime"
 	"testing"
 	"time"
 )
 
 func BenchmarkRaytracer(be *testing.B) {
-	for i := 0; i < be.N; i++ {
+	for i := 0; i < 5; i++ {
 		numcpu := runtime.NumCPU()
 		runtime.GOMAXPROCS(numcpu)
 
 		h := new(Helper.Helper)
 		width := 640
 		height := 480
-		filename := "out.png"
+		filename := path.Join(os.TempDir(), "out_"+time.Now().Format("20060102150405")+".png")
 
-		f, _ := os.Create("logfile")
-		log.SetOutput(f)
-		log.Println("################################")
-		log.Println("start rendering")
+		log.Println("Start rendering: ", filename)
 
 		sphere1 := objects.NewSphere(objects.NewVector(0.0, 0.0, 1.0), 1.0)
-		sphere2 := objects.NewSphere(objects.NewVector(-2.5, 0.0, -0.75), 1.0)
+		sphere2 := objects.NewSphere(objects.NewVector(float64(-i)-2.5, 0.0, -0.75), 1.0)
 		plane := objects.NewPlane(objects.NewVector(0.0, -1.0, 0.0), objects.NewVector(0.0, 1.0, 0.0))
 
+		basicBlue := objects.NewVector(0.54, 0.60, 0.90)
+		neonYellow := objects.NewVector(1.0, 2.0, 0.0)
+
 		grid1 := scene.NewGrid(objects.NewVector(2.0, 1.00, -1.0), objects.NewVector(2.0, -0.50, 1.0))
-		ball1 := scene.NewBall(sphere1, objects.NewVector(0.54, 0.60, 0.90), 0.9, 4.0, 30.0, 0.125)
-		ball2 := scene.NewBall(sphere2, objects.NewVector(1.0, 2.0, 0.0), 0.9, 4.0, 30.0, 0.125)
+		ball1 := scene.NewBall(sphere1, basicBlue, 0.9, 4.0, 30.0, 0.125)
+		ball2 := scene.NewBall(sphere2, neonYellow, 0.9, 4.0, 30.0, 0.125)
 
 		surface1 := scene.NewSurface(plane, objects.NewVector(1.0, 1.0, 1.0), 1.0, 1.0, 8.0, 0.05)
 		light1 := scene.NewLight(objects.NewVector(1.0, 4.0, 0.5), objects.NewVector(1.0, 1.0, 1.0))
